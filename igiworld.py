80b432cec53017f6c7f167017a4b982229912678
Shailendra Gupta <guptas199614@gmail.com>
2017-09-09 17:47:34 +0530

    Update igiworld.py

--- igiworld.py
+++ igiworld.py
@@ -3,57 +3,77 @@
 import sys
 import urllib2
 import csv
-import MySQLdb
-import json
+import re
 from bs4 import BeautifulSoup
 from difflib import SequenceMatcher
 
-result = urllib2.urlopen("http://www.igiworldwide.com/searchreport_postreq.php?r=" + str(234625698)).read()
-soup = BeautifulSoup(result, "html.parser")
-table = soup.find('table')
-rows = table.findAll('tr')
-values = []
-for row in rows[1:] :
-	val = []
-	val.append( row.findAll('td')[0].getText().encode("utf-8").strip() )
-	val.append( row.findAll('td')[1].getText().encode("utf-8").strip() )
-	values.append( val )
+#################### Importing Liraries ########################
 
-print json.dumps(values)
-file_obj = open("test.output","w")
-for item in values:
-	file_obj.write("%s\n" % item)
+if ( len(sys.argv) != 3 ) :
+    print "Only 2 arguments are allowed"
+    quit()
 
-print SequenceMatcher(None, "shailendra".lower(), "Shailendra".lower()).ratio()
+if ( sys.argv[1] > sys.argv[2] ) :
+    print "Invalid Arguments"
+    quit()
 
+#################### Checking Arguments ########################
 
+MEASUREMENT_REGEX = r"[0-9\.]{4}"
+REQUEST_URL = "http://www.igiworldwide.com/searchreport_postreq.php?r="
+PARAM_1 = int(sys.argv[1])
+PARAM_2 = int(sys.argv[2])
+REPORT_NO_LIST = list(range(PARAM_1 , PARAM_2 + 1))
 
+##################### Global Constants #########################
 
+def getDictValue(dict_param, str):
+    if str in dict_param:
+        return dict_param[str]
+    else:
+        return None
 
+################### User Defined Functions ######################
 
-# if ( len(sys.argv) != 3 ) :
-# 	print "Only 2 arguments are allowed"
-# 	quit()
+for report_no in REPORT_NO_LIST :
+    raw_data = urllib2.urlopen(REQUEST_URL + str(report_no)).read()
+    html_soup = BeautifulSoup(raw_data, "html.parser")
+    table_data_soup = html_soup.find('table')
 
-# if ( sys.argv[1] > sys.argv[2] ) :
-# 	print "Invalid Arguments"
-# 	quit()
+    if table_data_soup is None:
+        continue
+    
+    rows_soup = table_data_soup.findAll('tr')
+    rows_list = []
 
+    for row in rows_soup[1:] :
+        tmp = []
+        key = row.findAll('td')[0].getText().encode("utf-8").strip().lower()
+        value = row.findAll('td')[1].getText().encode("utf-8").strip().lower().replace('\xc2\xb0', '')
+        tmp.append( key )
+        tmp.append( value )
+        rows_list.append(tmp)
 
-# paramList = list(range( int(sys.argv[1]), int(sys.argv[2])+1 ))
+    rows_dict = dict(rows_list)
+    measurements_list = re.findall(MEASUREMENT_REGEX, rows_dict['measurements'])
+    print measurements_list
 
-
-# for reportNo in paramList :
-# 	#result = urllib2.urlopen("http://www.igiworldwide.com/searchreport_postreq.php?r=" + str(reportNo)).read()
-# 	result = urllib2.urlopen("http://www.igiworldwide.com/searchreport_postreq.php?r=" + str(234625698)).read()
-# 	soup = BeautifulSoup(result, "html.parser")
-# 	table = soup.find('table')
-# 	if table is None:
-# 		continue
-# 	else:
-# 		rows = table.findAll('tr')
-# 		values = []
-# 		for row in rows[1:] :
-# 			values.append( row.findAll('td')[1].getText().strip() )
-
-# 		file.writerow(values)
+    req_rows_dict = {
+        'report_no': getValueFromDict( rows_dict ,'report number' ),
+        'shape_cut': getValueFromDict( rows_dict , 'shape and cut' ),
+        'carat_weight': getValueFromDict( rows_dict , 'carat weight' ),
+        'color_grade': getValueFromDict( rows_dict , 'color grade' ),
+        'clarity_grade': getValueFromDict( rows_dict , 'clarity grade' ),
+        'polish': getValueFromDict( rows_dict , 'polish' ),
+        'symmetry': getValueFromDict( rows_dict , 'symmetry' ),
+        'table_size': getValueFromDict( rows_dict , 'table size' ),
+        'total_depth': getValueFromDict( rows_dict , 'total depth' ),
+        'fluorescence': getValueFromDict( rows_dict , 'fluorescence' ),
+        'width': measurements_list[0],
+        'length': measurements_list[1],
+        'depth': measurements_list[2],
+        'shape': 'shape',
+        'cut': 'cut',
+        'cut_grade': getValueFromDict( rows_dict , 'cut grade' ),
+        'measurement': getValueFromDict( rows_dict , 'measurements' )
+    }
