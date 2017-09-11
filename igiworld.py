39f4b63a750894d0e44226096de425a796e0c945
Shailendra Gupta <guptas199614@gmail.com>
2017-09-11 13:10:30 +0530

    Update igiworld.py

--- igiworld.py
+++ igiworld.py
@@ -4,6 +4,7 @@ import sys
 import urllib2
 import csv
 import re
+import time
 from bs4 import BeautifulSoup
 from difflib import SequenceMatcher
 
@@ -24,16 +25,33 @@ REQUEST_URL = "http://www.igiworldwide.com/searchreport_postreq.php?r="
 PARAM_1 = int(sys.argv[1])
 PARAM_2 = int(sys.argv[2])
 REPORT_NO_LIST = list(range(PARAM_1 , PARAM_2 + 1))
+REQ_FEILDS = ["report_no","shape_cut","carat_weight","color_grade","clarity_grade","polish","symmetry","table_size","total_depth","fluorescence", "width", "length", "depth", "measurement", "shape", "cut", "cut_grade"]
+#CSV_FILENAME = "csv_" + time.strftime("%d-%m-%Y_%H-%M-%S") + ".csv"
+CSV_FILENAME = "test.csv"  ## development only
 
 ##################### Global Constants #########################
 
-def getDictValue(dict_param, str):
+def getValueFromDict(dict_param, str):
     if str in dict_param:
         return dict_param[str]
     else:
         return None
 
+def populateRowsList(raw_rows):
+    rows_list = []
+    for row in raw_rows[1:] :
+        tmp = []
+        key = row.findAll('td')[0].getText().encode("utf-8").strip().lower()
+        value = row.findAll('td')[1].getText().encode("utf-8").strip().lower().replace('\xc2\xb0', '')
+        tmp.append( key )
+        tmp.append( value )
+        rows_list.append(tmp)
+    return rows_list
+
 ################### User Defined Functions ######################
+
+csv_file_handler = csv.writer(open(CSV_FILENAME, "w"))
+csv_file_handler.writerow(REQ_FEILDS)
 
 for report_no in REPORT_NO_LIST :
     raw_data = urllib2.urlopen(REQUEST_URL + str(report_no)).read()
@@ -41,39 +59,50 @@ for report_no in REPORT_NO_LIST :
     table_data_soup = html_soup.find('table')
 
     if table_data_soup is None:
+        print str(report_no) + " Failure"
         continue
-    
-    rows_soup = table_data_soup.findAll('tr')
-    rows_list = []
+        
+    try:
+        rows_soup = table_data_soup.findAll('tr')
+        rows_list = populateRowsList(rows_soup)
+        rows_dict = dict(rows_list)
+        measurements_list = re.findall(MEASUREMENT_REGEX, rows_dict['measurements'])
+        shape_cut_list = rows_dict['shape and cut'].split()
 
-    for row in rows_soup[1:] :
-        tmp = []
-        key = row.findAll('td')[0].getText().encode("utf-8").strip().lower()
-        value = row.findAll('td')[1].getText().encode("utf-8").strip().lower().replace('\xc2\xb0', '')
-        tmp.append( key )
-        tmp.append( value )
-        rows_list.append(tmp)
+        req_rows_list = []
+        req_rows_dict = {
+            'report_no': getValueFromDict( rows_dict ,'report number' ),
+            'shape_cut': getValueFromDict( rows_dict , 'shape and cut' ),
+            'carat_weight': getValueFromDict( rows_dict , 'carat weight' ).replace('carat','').strip(),
+            'color_grade': getValueFromDict( rows_dict , 'color grade' ),
+            'clarity_grade': getValueFromDict( rows_dict , 'clarity grade' ),
+            'polish': getValueFromDict( rows_dict , 'polish' ),
+            'symmetry': getValueFromDict( rows_dict , 'symmetry' ),
+            'table_size': getValueFromDict( rows_dict , 'table size' ).replace('%','').strip(),
+            'total_depth': getValueFromDict( rows_dict , 'total depth' ).replace('%','').strip(),
+            'fluorescence': getValueFromDict( rows_dict , 'fluorescence' ),
+            'width': measurements_list[0],
+            'length': measurements_list[1],
+            'depth': measurements_list[2],
+            'shape': shape_cut_list[0],
+            'cut': shape_cut_list[1],
+            'cut_grade': getValueFromDict( rows_dict , 'cut grade' ),
+            'measurement': getValueFromDict( rows_dict , 'measurements' )
+        }
 
-    rows_dict = dict(rows_list)
-    measurements_list = re.findall(MEASUREMENT_REGEX, rows_dict['measurements'])
-    print measurements_list
+        for req in REQ_FEILDS :
+            req_rows_list.append(req_rows_dict[req])
 
-    req_rows_dict = {
-        'report_no': getValueFromDict( rows_dict ,'report number' ),
-        'shape_cut': getValueFromDict( rows_dict , 'shape and cut' ),
-        'carat_weight': getValueFromDict( rows_dict , 'carat weight' ),
-        'color_grade': getValueFromDict( rows_dict , 'color grade' ),
-        'clarity_grade': getValueFromDict( rows_dict , 'clarity grade' ),
-        'polish': getValueFromDict( rows_dict , 'polish' ),
-        'symmetry': getValueFromDict( rows_dict , 'symmetry' ),
-        'table_size': getValueFromDict( rows_dict , 'table size' ),
-        'total_depth': getValueFromDict( rows_dict , 'total depth' ),
-        'fluorescence': getValueFromDict( rows_dict , 'fluorescence' ),
-        'width': measurements_list[0],
-        'length': measurements_list[1],
-        'depth': measurements_list[2],
-        'shape': 'shape',
-        'cut': 'cut',
-        'cut_grade': getValueFromDict( rows_dict , 'cut grade' ),
-        'measurement': getValueFromDict( rows_dict , 'measurements' )
-    }
+        csv_file_handler.writerow(req_rows_list)
+        print str(report_no) + " Success"
+    except:
+        print str(report_no) + " Failure"
+        EX_FILE = str(replaceeport_no) + ".txt"
+        file_handler = open(EX_FILE,"w")
+        file_handler.write(table_data_soup)
+        file_handler.close()
+
+
+#################################################################
+
+# print SequenceMatcher(None, "shailendra".lower(), "Shailendra".lower()).ratio()
